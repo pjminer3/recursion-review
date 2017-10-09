0572818de15200740c3903be4c2efcd8b2862821
pjminer3 <patrickminer318@gmail.com>
2017-10-09 16:27:13 -0700

    finished required recursion tasks

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,56 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) { 
+  var result = [];
+  
+  var recursion = function(className, node) {
+    node = node || document.body;
+    // if node has className in classList, then push node into result
+    // if node has children, then cycle through children
+    
+    if (node.classList !== undefined && node.classList.contains(className)) {
+      result.push(node);
+    }
+    if (node.hasChildNodes()) {
+      var childNodes = node.childNodes;
+      for (var i = 0; i < childNodes.length; i++) {
+        recursion(className, childNodes[i]);
+      }
+    }
+  };
+  recursion(className);
+  return result;
 };
+
+/*
+  // declare result variable will be array
+  var result = [];
+  // loop through all child nodes
+    // if desired class => push it to result
+    // if node has child nodes, 
+        // result.concat(getElementsByClassName(childNode))
+  // return result
+
+  
+  var node = arguments[1] || document.body;
+  var nodeList = node.childNodes;
+  //console.log('here is nodelist: ', nodeList);
+  
+  
+  for (var i = 0; i < nodeList.length; i++) {
+    if (nodeList[i].classList !== undefined && nodeList[i].classList.contains(className)) {
+      // console.log('in if loop: ', nodeList[i]);
+      // console.log('has childNodes: ', nodeList[i].hasChildNodes());
+      result.push(nodeList[i]);
+    }
+    // if (nodeList[i].hasChildNodes()) {
+    //   // console.log('in second if loop: ', result);
+    //   result = result.concat(getElementsByClassName(className, nodeList[i]));
+    //   // console.log('after concat: ', result);
+    // }
+  }
+  console.log(result);
+  return result;
+  
+  */
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,25 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === null) {
+    return 'null';
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"'; 
+  } else if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj.toString();
+  } else if (Array.isArray(obj)) {
+    var arrayString = '[';
+    for (var i = 0; i < obj.length; i++) {
+      arrayString += stringifyJSON(obj[i]) + ',';
+    }
+    return arrayString.slice(0, Math.max(arrayString.length - 1, 2)) + ']';
+  } else /*objects*/{
+    var objectString = '{';
+    for (var key in obj) {
+      if (typeof obj[key] !== 'undefined' && typeof obj[key] !== 'function') {
+        objectString += ('"' + key + '"' + ':' + stringifyJSON(obj[key]) + ',');
+      }
+    }
+    return objectString.slice(0, Math.max(objectString.length - 1, 2)) + '}';
+  }
 };
